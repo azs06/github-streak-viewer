8aa4b0c6865d55d1d40ae7b21695f563fd5f7801
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-02-22 00:42:04 +0600

    cleanup

--- __app/constant.js
+++
@@ -1,4 +0,0 @@
-export const CONTRIBUTION_DATA = "contribution_data";
-export const CONTRIBUTION_KEY = "github_contribution";
-export const FIRST_COMMIT_KEY = "github_first_commit";
-export const ALL_TIME_CONTRIBUTION_KEY = "github_all_time_contribution";

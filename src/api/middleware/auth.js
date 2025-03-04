eeba67797666bce9b1926f1ea5050d180b4c9839
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-03-05 02:26:58 +0600

    added user control

---
+++ src/api/middleware/auth.js
@@ -0,0 +1,13 @@
+import { ALLOWED_USERS } from "../../config/config.js";
+
+const addUserControl = (req, res, next) => {
+  const { username } = req.params;
+  if (
+    ALLOWED_USERS &&
+    ALLOWED_USERS.length > 0 &&
+    !ALLOWED_USERS.includes(username)
+  ) {
+    return res.status(403).send("User not allowed");
+  }
+  next();
+};
--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,7 @@ dotenv.config({ path: path.resolve(import.meta.dirname, "../../.env") });
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN || "";
 const PORT = process.env.PORT || 3002;
+const ALLOWED_USERS = process.env.ALLOWED_USERS || [];
 
 
-export { GITHUB_TOKEN, PORT };
+export { GITHUB_TOKEN, PORT, ALLOWED_USERS };

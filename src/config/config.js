d7d1e4feba23953c306b0c50f905788a02cb0ef6
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-03-05 02:51:32 +0600

    updated path

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
-import path from "path";
 
-dotenv.config({ path: path.resolve(import.meta.dirname, "../../.env") });
+dotenv.config();
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN || "";
 const PORT = process.env.PORT || 3002;
